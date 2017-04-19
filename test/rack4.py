e4c9fbac26f08e3797328a225a9659fcd9dd69e8
jobvink <jmcvink@gmail>
2017-04-19 13:38:54 +0200

    'test'

--- test/rack4.py
+++ test/rack4.py
@@ -279,6 +279,7 @@ def main():
             exec ("pin = pin%i" % i)
             if not gpio.input(pin):
                 set_hanger_color(system_sizes[i])
+                # test
                 print 'Knop voor maat %s ingedrukt' % system_sizes[i]
 
 
